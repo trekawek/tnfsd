09bc9ea8e509036d44f17bde6d21be2d8a6eaa32
Noah Burney <noah.burney@gmail.com>
2025-01-05 19:43:55 -0600

    feat(gui): add Go wrapper

--- gui/main.go
+++ gui/main.go
@@ -7,6 +7,11 @@ import (
 )
 
 func main() {
+<<<<<<< HEAD
 	fmt.Println("Starting server")
 	gotnfsd.StartServer(".")
+=======
+	gotnfsd.StartServer(".")
+	fmt.Println(".")
+>>>>>>> b804bc8 (feat(gui): add Go wrapper)
 }
