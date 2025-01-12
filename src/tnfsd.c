e1210d9427289d62b1322f15fedf42d07ef281cc
Tomek Rękawek <trekawek@gmail.com>
2025-01-12 14:49:58 +0100

    Make the indention consistent in modified files.

--- src/tnfsd.c
+++ src/tnfsd.c
@@ -17,41 +17,41 @@ void tnfsd_init()
 
 void tnfsd_init_logs(int log_output_fd)
 {
-    FILE* log_output = fdopen(log_output_fd, "w");
-    log_init(log_output);
+	FILE* log_output = fdopen(log_output_fd, "w");
+	log_init(log_output);
 }
 
 int tnfsd_start(const char* path, int port, bool read_only)
 {
 	LOG("Starting tnfsd version %s on port %d using root directory \"%s\"\n", version, port, path);
-    if (read_only)
-    {
-        LOG("The server runs in read-only mode. TNFS clients can only list and download files.\n");
-    }
-    else
-    {
-        LOG("The server runs in read-write mode. TNFS clients can upload and modify files. Use -r to enable read-only mode.\n");
-    }
+	if (read_only)
+	{
+		LOG("The server runs in read-only mode. TNFS clients can only list and download files.\n");
+	}
+	else
+	{
+		LOG("The server runs in read-write mode. TNFS clients can upload and modify files. Use -r to enable read-only mode.\n");
+	}
 
-    if (tnfs_setroot(path) < 0)
-    {
+	if (tnfs_setroot(path) < 0)
+	{
 		LOG("Invalid root directory: %s\n", path);
-        return TNFSD_ERR_INVALID_DIR;
-    }
+		return TNFSD_ERR_INVALID_DIR;
+	}
 	tnfs_event_init();        /* initialize event system */
 	if (tnfs_sockinit(port) < 0)  /* initialize communications */
-    {
-        LOG("Can't bind port %d\n", port);
-        return TNFSD_ERR_SOCKET_ERROR;
-    }      
+	{
+		LOG("Can't bind port %d\n", port);
+		return TNFSD_ERR_SOCKET_ERROR;
+	}      
 	auth_init(read_only);     /* initialize authentication */
 	tnfs_mainloop();          /* run */
-    tnfs_event_close();
-    return 0;
+	tnfs_event_close();
+	return 0;
 }
 
 void tnfsd_stop()
 {
-    LOG("Stopping tnfsd server.\n");
-    tnfs_sockclose();
+	LOG("Stopping tnfsd server.\n");
+	tnfs_sockclose();
 }
